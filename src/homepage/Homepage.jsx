48b222c5f10cd0d7d67dbb2a8a5ae5249b84a2cc
Lindsey Ipson <lindseyipson@gmail.com>
2024-03-21 03:42:39 -0700

    change text on homepage

--- src/homepage/Homepage.jsx
+++ src/homepage/Homepage.jsx
@@ -8,11 +8,13 @@ function Homepage() {
         <h1>Welcome to Poetry Peers!</h1>
         <h3>Start analyzing poetry alongside the Poetry Peers community today!</h3>
         
-        <p>Explore new poems through the Poems tab, where you can read twenty new random poems each time!</p>
+        <p>Explore new poems in the Poems tab, where you can read twenty new random poems each time, or search by poem title.</p>
           
-        <p>If you find any themes you'd like to tag, simply highlight the relevant lines and provide your analysis for other members to see!</p>
+        <p>If you find any themes you'd like to tag in any poem, simply highlight the relevant lines and provide your analysis for other members to see!</p>
            
-        <p>Or, explore different themes in poetry through the Themes tab. You can review or delete any tags you've made through the Contributions tab.</p>
+        <p>You can also explore different themes or search for specific themes others have tagged in the Themes tab.</p>
+        
+        <p>To review or delete tags you've made, see the Contributions tab.</p>
       </div>
     </div>
   );
